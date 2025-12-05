fa47cfc2115948859f160ba1006849ee7e2a6c44
125113052 <125113052@nitkkr.ac.in>
2025-12-05 16:00:08 +0530

    43.c

--- 43.c
+++ 43.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
   FILE*fp;
-fp=fopen("text1.txt","w"):
+fp=fopen("text1.txt","w");
 char text[100];
 fgets(text,sizeof(text),stdin);
-fprintf(fp,%s,text);
-fclsoe(fp);
+fprintf(fp,"%s",text);
+fclose(fp);
 printf("maryadit 125113052");
 return 0;
 }
+
