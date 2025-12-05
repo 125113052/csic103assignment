abdcda81e6b5e4222dfe0fff52d32939bfb1b771
125113052 <125113052@nitkkr.ac.in>
2025-12-05 15:58:56 +0530

    43.c

---
+++ 43.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  FILE*fp;
+fp=fopen("text1.txt","w"):
+char text[100];
+fgets(text,sizeof(text),stdin);
+fprintf(fp,%s,text);
+fclsoe(fp);
+printf("maryadit 125113052");
+return 0;
+}
